479c65cf1ce0cef4736639835c3b0fd00d4c4553
dltpwns0 <dltpwns6@naver.com>
2023-12-30 23:01:38 +0900

    front: 고정 스타일 추가

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,124 @@
+import { css } from "styled-components";
+
+const fontFormat = {
+  largetitle: css`
+    font-size: 34px;
+    line-height: 41px;
+    font-weight: 400;
+  `,
+  title1: css`
+    font-size: 28px;
+    line-height: 34px;
+    font-weight: 400;
+  `,
+  title2: css`
+    font-size: 22px;
+    line-height: 28px;
+    font-weight: 400;
+  `,
+  title3: css`
+    font-size: 20px;
+    line-height: 25px;
+    font-weight: 400;
+  `,
+  headline: css`
+    font-size: 17px;
+    line-height: 22px;
+    font-weight: 600;
+  `,
+  body: css`
+    font-size: 17px;
+    line-height: 24px;
+    font-weight: 400;
+  `,
+  content: css`
+    font-size: 17px;
+    line-height: 22px;
+    font-weight: 400;
+  `,
+  callout: css`
+    font-size: 16px;
+    line-height: 21px;
+    font-weight: 400;
+  `,
+  subhead: css`
+    font-size: 15px;
+    line-height: 20px;
+    font-weight: 400;
+  `,
+  footnote: css`
+    font-size: 13px;
+    line-height: 18px;
+    font-weight: 400;
+  `,
+  caption1: css`
+    font-size: 12px;
+    line-height: 16px;
+    font-weight: 400;
+  `,
+  caption2: css`
+    font-size: 11px;
+    line-height: 13px;
+    font-weight: 400;
+  `,
+};
+
+const color = {
+  primary: css`
+    color: #b9bbbe;
+  `,
+  secondary: css`
+    color: #72767d;
+  `,
+  neutral: css`
+    color: #ffffff;
+  `,
+  systemDefault: css`
+    color: #007aff;
+  `,
+  systemWarning: css`
+    color: #ff3b30;
+  `,
+  backgroundPrimary: css`
+    background-color: #2a2d31;
+  `,
+};
+
+const flex = {
+  rowCenterCenter: css`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  `,
+  columnCenterCenter: css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `,
+  columnCenterStart: css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  `,
+};
+
+const position = {
+  center: css`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  `,
+};
+
+const theme = {
+  color,
+  flex,
+  position,
+  fontFormat,
+};
+
+export default theme;
