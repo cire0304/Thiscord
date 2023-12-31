084ab2833684dc38cfff20a90fadc7b1b6be36b4
dltpwns0 <dltpwns6@naver.com>
2024-01-01 00:54:47 +0900

    back: 초기세팅

---
+++ backend/Thiscode/src/main/java/com/example/thiscode/ThiscodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.thiscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThiscodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThiscodeApplication.class, args);
+    }
+
+}
