1d8cf83caa6599743ccdb1e957f86be328a9da19
dltpwns0 <dltpwns6@naver.com>
2024-01-11 14:39:25 +0900

    front: 코드 스타일 변경

--- frontend/src/pages/workplace/containers/main/components/addFreind.tsx
+++ frontend/src/pages/workplace/containers/main/components/addFreind.tsx
@@ -6,6 +6,78 @@ import { Input } from "../../../../../components/input";
 import Button from "../../../../../components/button";
 import FriendReqeust from "../../../../../api/friend";
 
+export default function AddFriend() {
+  const [buttonActive, setButtonActive] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [hidden, setHidden] = useState(true);
+  const [warningMessage, setWarningMessage] = useState("");
+
+  const nicknameAnduserCodeRef = useRef<HTMLInputElement>(null);
+
+  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const value = e.target.value;
+    setInputValue(value);
+    if (value.length > 0) setButtonActive(true);
+    else setButtonActive(false);
+  };
+
+  const buttonClickHandler = () => {
+    const nickname = inputValue.split("#")[0];
+    const userCode = inputValue.split("#")[1];
+    console.log(nickname, userCode);
+    if (
+      nickname === undefined ||
+      nickname.length < 1 ||
+      userCode === undefined ||
+      userCode.length < 1
+    ) {
+      setWarningMessage(
+        "형식이 잘못된거 같아요. 올바르게 입력했는지 확인해보세요."
+      );
+      setHidden(false);
+      return;
+    }
+
+    const addFreindRequest = async () => {
+      const data = { nickname, userCode };
+      const res = await FriendReqeust.addFriend(data);
+
+      if (res.status !== 200) {
+        setWarningMessage(res.data);
+        setHidden(false);
+      }
+    };
+    addFreindRequest();
+  };
+
+  return (
+    <Container>
+      <Header>
+        <Title>친구 추가하기</Title>
+        <SubTitle>
+          시레#123456 와 같이 사용자명을 사용하여 친구를 추가할 수 있어요
+        </SubTitle>
+      </Header>
+      <Body>
+        <InputWrapper>
+          <Input
+            onChange={(e) => inputChangeHandler(e)}
+            ref={nicknameAnduserCodeRef}
+          />
+          <InputButton
+            onClick={() => buttonClickHandler()}
+            active={buttonActive}
+          >
+            친구 요청 보내기
+          </InputButton>
+        </InputWrapper>
+        <WarningMessage hidden={hidden}>{warningMessage}</WarningMessage>
+      </Body>
+    </Container>
+  );
+};
+
 const Container = styled.div`
   padding: 20px 20px;
   border-bottom: 1px solid #404147;
@@ -79,77 +151,3 @@ const InputButton = styled(Button)<{ active: boolean }>`
     background-color: #2e3136;
   }
 `;
-
-const AddFriend = () => {
-  const [buttonActive, setButtonActive] = useState(false);
-  const [inputValue, setInputValue] = useState("");
-  const [hidden, setHidden] = useState(true);
-  const [warningMessage, setWarningMessage] = useState("");
-
-  const nicknameAnduserCodeRef = useRef<HTMLInputElement>(null);
-
-  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    const value = e.target.value;
-    setInputValue(value);
-    if (value.length > 0) setButtonActive(true);
-    else setButtonActive(false);
-  };
-
-  const buttonClickHandler = () => {
-    const nickname = inputValue.split("#")[0];
-    const userCode = inputValue.split("#")[1];
-    console.log(nickname, userCode);
-    if (
-      nickname === undefined ||
-      nickname.length < 1 ||
-      userCode === undefined ||
-      userCode.length < 1
-    ) {
-      setWarningMessage(
-        "형식이 잘못된거 같아요. 올바르게 입력했는지 확인해보세요."
-      );
-      setHidden(false);
-      return;
-    }
-
-    const addFreindRequest = async () => {
-      const data = { nickname, userCode };
-      const res = await FriendReqeust.addFriend(data);
-
-      if (res.status !== 200) {
-        setWarningMessage(res.data);
-        setHidden(false);
-      }
-    };
-    addFreindRequest();
-  };
-
-  return (
-    <Container>
-      <Header>
-        <Title>친구 추가하기</Title>
-        <SubTitle>
-          시레#123456 와 같이 사용자명을 사용하여 친구를 추가할 수 있어요
-        </SubTitle>
-      </Header>
-      <Body>
-        <InputWrapper>
-          <Input
-            onChange={(e) => inputChangeHandler(e)}
-            ref={nicknameAnduserCodeRef}
-          />
-          <InputButton
-            onClick={() => buttonClickHandler()}
-            active={buttonActive}
-          >
-            친구 요청 보내기
-          </InputButton>
-        </InputWrapper>
-        <WarningMessage hidden={hidden}>{warningMessage}</WarningMessage>
-      </Body>
-    </Container>
-  );
-};
-
-export default AddFriend;
