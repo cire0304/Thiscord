d76bbcbee2a2b19604441ffb59c576ecc77cbed4
dltpwns0 <dltpwns6@naver.com>
2024-01-11 14:39:44 +0900

    front: firebase 의존성 추가

---
+++ frontend/src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClVGhUnznOCJsY-eZOJ0BczESwbajxc9c",
+  authDomain: "react-chat-app-2633a.firebaseapp.com",
+  projectId: "react-chat-app-2633a",
+  storageBucket: "react-chat-app-2633a.appspot.com",
+  messagingSenderId: "728231955593",
+  appId: "1:728231955593:web:7cbf93d540f109957d4a58"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
