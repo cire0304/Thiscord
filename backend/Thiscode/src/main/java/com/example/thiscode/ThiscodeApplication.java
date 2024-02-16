64cd8e77ca64127ae4dd7800dac6e03224d24966
dltpwns0 <dltpwns6@naver.com>
2024-02-16 17:50:29 +0900

    back: 불필요한 의존성 제거

--- backend/Thiscode/src/main/java/com/example/thiscode/ThiscodeApplication.java
+++ backend/Thiscode/src/main/java/com/example/thiscode/ThiscodeApplication.java
@@ -3,11 +3,8 @@ package com.example.thiscode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableJpaAuditing
-@EnableMongoRepositories
 @SpringBootApplication
 public class ThiscodeApplication {
 
